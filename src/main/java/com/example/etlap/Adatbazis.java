58a99e9d3a2c7a56d26e542f0262995fc38c12ce
Domokos David <domokosdavid4@gmail.com>
2022-02-02 16:00:21 +0100

    adatbazis hozzaad

---
+++ src/main/java/com/example/etlap/Adatbazis.java
@@ -0,0 +1,12 @@
+package com.example.etlap;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adatbazis extends AdatbazisController{
+    Connection connection;
+    public Adatbazis() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
+    }
+}
--- src/main/java/com/example/etlap/EtlapController.java
+++ src/main/java/com/example/etlap/EtlapController.java
@@ -1,14 +1,23 @@
 package com.example.etlap;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class EtlapController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void felvetelButton(ActionEvent actionEvent) {
+
+    }
+
+    public void torlesButton(ActionEvent actionEvent) {
+
     }
 }
