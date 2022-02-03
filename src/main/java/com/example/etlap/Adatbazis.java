dfc9fe1702e55a04b366a166965ddce7fd95b1af
Domokos David <domokosdavid4@gmail.com>
2022-02-03 13:42:43 +0100

    tablazatba iras

--- src/main/java/com/example/etlap/Adatbazis.java
+++ src/main/java/com/example/etlap/Adatbazis.java
@@ -10,6 +10,20 @@ public class Adatbazis extends AdatbazisController{
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
     }
 
+    public List<Kategoria> getKategoria() throws SQLException {
+        List<Kategoria> osszesKategoria = new ArrayList<>();
+        String sql = "SELECT * FROM kategoriak";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()){
+            int id = resultSet.getInt("id");
+            String nev = resultSet.getString("nev");
+            Kategoria obj = new Kategoria(nev, id);
+            osszesKategoria.add(obj);
+        }
+        return osszesKategoria;
+    }
     public List<Etel> getEtelek() throws SQLException {
         List<Etel> etelek = new ArrayList<>();
         Statement statement = connection.createStatement();
@@ -27,6 +41,8 @@ public class Adatbazis extends AdatbazisController{
         }
         return etelek;
     }
+
+
     public int etelHozzaadas(String nev, String leiras, int ar, int kategoria) throws SQLException {
         PreparedStatement statement = connection.prepareStatement("INSERT INTO etlap(nev, leiras, ar, kategoria_id) VALUES(?,?,?,?)");
         statement.setString(1, nev);
--- src/main/java/com/example/etlap/EtlapController.java
+++ src/main/java/com/example/etlap/EtlapController.java
@@ -1,9 +1,13 @@
 package com.example.etlap;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class EtlapController {
@@ -24,7 +28,46 @@ public class EtlapController {
     @FXML
     private Adatbazis adatbazis;
 
+    public void init() throws SQLException {
+        columnNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        columnAr.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        columnKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
 
+        try {
+            adatbazis = new Adatbazis();
+            try {
+                List<Etel> etelList = adatbazis.getEtelek();
+                etelTableView.getItems().clear();
+                for (Etel item : etelList) {
+                    etelTableView.getItems().add(item);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        kategorias = adatbazis.getKategoria();
+        kategorias.add(0, new Kategoria("Nincs rendezve", 0));
+        ObservableList<Kategoria> kategoriaList = FXCollections.observableArrayList(kategorias);
+        kategoriaChoiceBox.setItems(kategoriaList);
+        kategoriaChoiceBox.getSelectionModel().selectFirst();
+
+        etelTableView.setRowFactory(etelTableView1 -> {
+            TableRow<Etel> sor = new TableRow<>();
+            sor.setOnMouseClicked(mouseEvent -> {
+                Etel etel = etelTableView.getSelectionModel().getSelectedItem();
+                String tablazatba = etel.getNev()+" - "+etel.getKategoria()+" - "+etel.getLeiras()+" - "+etel.getAr()+" Ft";
+                textFieldKivalaszt.setText(tablazatba);
+            });
+            return sor;
+        });
+
+
+
+
+
+    }
 
     public void felvetelButton(ActionEvent actionEvent) {
 
@@ -33,4 +76,6 @@ public class EtlapController {
     public void torlesButton(ActionEvent actionEvent) {
 
     }
+
 }
+
