b131185c431e397ad6056db74ef7034f2cb84966
Domokos David <domokosdavid4@gmail.com>
2022-02-03 12:55:39 +0100

    helloview id letrehoz

--- src/main/java/com/example/etlap/EtlapController.java
+++ src/main/java/com/example/etlap/EtlapController.java
@@ -2,9 +2,20 @@ package com.example.etlap;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class EtlapController {
+    @FXML
+    private TableView<Etel> etelTableView;
+    @FXML
+    private TableColumn<Etel, String> columnNev, columnKategoria;
+    @FXML
+    private TableColumn<Etel, Integer> columnAr;
+    @FXML
+    private TextField textFieldKivalaszt;
+    @FXML
+    private Spinner<Integer> szazalekSP, forintSP;
+
 
 
     public void felvetelButton(ActionEvent actionEvent) {
